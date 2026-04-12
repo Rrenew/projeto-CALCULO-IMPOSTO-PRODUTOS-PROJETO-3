669e4c014446759345cff38baa3ac379350e12fc
Decay <0_0o@tutamail.com>
2026-04-12 05:49:49 -0300

    Reformulação javascript, minha parte / eliel, função de adicionarP ready

--- script/script.js
+++ script/script.js
@@ -6,3 +6,31 @@ const impostos = {
  4: 12,
  5: 17,
 };
+
+function addProduto() { 
+    e.preventDefault();
+    const nome = document.getElementById("produto").value;
+    const descricao = document.getElementById("caracteristicas").value;
+    const preco = parseFloat(document.getElementById("preco").value);
+    const medida = document.getElementById("unidade").value;
+    const tipo = document.querySelector('input[name="tipoProduto"]:checked')?.value;
+
+    if (!nome || !descricao || isNaN(preco) ||preco <=0 ||!medida || !tipo) {
+        alert("preencha todos os campos corretamente.");
+        return;
+    }
+    const Novproduto = {
+    id: Date.now(),
+    produto: nome,
+    caracteristicas: descricao,
+    valorUnitario: preco,
+    unidade: medida,
+    tipoProduto: parseInt(tipo),
+    quantidade: 1    
+    };
+    produtos.push(Novproduto);
+    document.getElementById("produtoForm").reset();
+    renderizar();
+}   
+
+
